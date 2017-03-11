2d33ba76dc0cc5af6f6d73e3b8633b6c890b4078
Alonso Robles <alonso@alonsorobles.com>
2017-03-11 12:27:43 -0600

    Initialize store and provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-import {Router, browserHistory} from 'react-router';
-import routes from './routes';
-import './styles/style.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "babel-polyfill";
+import React from "react";
+import {render} from "react-dom";
+import configureStore from "./store/configureStore";
+import {Provider} from "react-redux";
+import {Router, browserHistory} from "react-router";
+import routes from "./routes";
+import "./styles/style.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const store = configureStore(); //can pass initial state from local storage or server here
 
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>,
   document.getElementById('app')
 );
