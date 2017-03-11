e99617b9162225baaf992958dbfdbcd30ff6344d
Alonso Robles <alonso@alonsorobles.com>
2017-03-10 18:00:55 -0600

    Updated entry point and core style

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ class App extends React.Component {
     return (
       <div className="container-fluid">
         <p>Header here...</p>
-        {this.props.childern}
+        {this.props.children}
       </div>
     );
   }
 }
 
 App.propTypes = {
-  childern: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired
 };
 
 export default App;
