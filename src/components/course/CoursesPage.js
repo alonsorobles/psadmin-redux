9e72c8c46630bb24d4834c738e8d54b588399def
Alonso Robles <alonso@alonsorobles.com>
2017-03-11 14:12:50 -0600

    Removed form from course page to start separating concerns

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -12,33 +12,14 @@ class CoursesPage extends React.Component {
 
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: {title: ''}
-    };
-
-    this.handleTitleChange = this.handleTitleChange.bind(this);
-    this.handleClickSave = this.handleClickSave.bind(this);
   }
 
-  handleTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  handleClickSave() {
-    this.props.actions.createCourse(this.state.course);
-  }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(CoursesPage.courseRow)}
-        <h2>Add Course</h2>
-        <input type="text" onChange={this.handleTitleChange} value={this.state.course.title}/>
-        <input type="submit" value="Save" onClick={this.handleClickSave}/>
       </div>
     );
   }
