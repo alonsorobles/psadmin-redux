f976bddf7c9d0b0ea51092738fb5a3516ec7f131
Alonso Robles <alonso@alonsorobles.com>
2017-03-12 14:41:46 -0500

    Added link to authors page in header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,6 +8,7 @@ const Header = ({loading}) => {
         <ul className="nav navbar-nav">
           <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
           <li><Link to="courses" activeClassName="active">Courses</Link></li>
+          <li><Link to="authors" activeClassName="active">Authors</Link></li>
           <li><Link to="about" activeClassName="active">About</Link></li>
           {loading && <li><p className="navbar-text"><i className="fa fa-refresh fa-spin fa-fw" aria-hidden="true"/> Loading</p></li>}
         </ul>
