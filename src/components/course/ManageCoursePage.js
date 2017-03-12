fdf91302666fa86a3f80a4b3002c6a612085769d
Alonso Robles <alonso@alonsorobles.com>
2017-03-11 20:39:16 -0600

    Added form hydrating... this is buggy.

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -52,8 +52,20 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
-function mapStateToProps(state) {
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course.length) return course[0];
+  return null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id; // from the path /course/:id
+
   let course = {id: '', watchRef: '', title: '', authorId: '', length: '', category: ''};
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
