e81e75d8cc314ed983ce469ce9cf585a1b29e75b
Alonso Robles <alonso@alonsorobles.com>
2017-03-12 18:21:15 -0500

    Wired up another TextInput for author.lastName

--- src/components/author/AuthorForm.js
+++ src/components/author/AuthorForm.js
@@ -2,11 +2,20 @@ import React, {PropTypes} from "react";
 import TextInput from "../common/TextInput";
 
 const AuthorForm = ({author, onChange, errors}) => {
-    return (
-        <form>
-          <TextInput name="firstName" label="First Name" onChange={onChange} value={author.firstName} error={errors.firstName}/>
-        </form>
-    );
+  return (
+    <form>
+      <TextInput name="firstName"
+                 label="First Name"
+                 onChange={onChange}
+                 value={author.firstName}
+                 error={errors.firstName}/>
+      <TextInput name="lastName"
+                 label="Last Name"
+                 onChange={onChange}
+                 value={author.lastName}
+                 error={errors.lastName}/>
+    </form>
+  );
 };
 
 AuthorForm.propTypes = {
