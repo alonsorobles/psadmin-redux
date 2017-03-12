2820af12a7652a41eefb570c8fde06898436d5c7
Alonso Robles <alonso@alonsorobles.com>
2017-03-12 11:32:19 -0500

    Simple redux reducer test

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 * Consider adding a library like "Reselect" to memoize function calls if we are implementing expensive mapStateToProp functions
 * Look into redux-promise when it becomes more stable and look at redux-saga as well as an alternative to redux-thunk
 * Testing actions may not be worth while since logic is very simple yielding low value tests.
+* Consider looking into "Redux Test Recorder" to automate testing redux reducers
+
---
+++ src/reducers/courseReducer.test.js
@@ -0,0 +1,48 @@
+import expect from "expect";
+import {describe, it} from "mocha";
+import courseReducer from "./courseReducer";
+import * as actions from "../actions/courseActions";
+import * as types from "../actions/actionTypes";
+
+describe('Course Reducer', () => {
+  it('should add course when passed ' + types.CREATE_COURSE_SUCCESS, () => {
+    // arrange
+    const initialState = [
+      {title: 'A'},
+      {title: 'B'}
+    ];
+    const newCourse = {title: 'C'};
+    const action = actions.createCourseSuccess(newCourse);
+
+    // act
+    const newState = courseReducer(initialState, action);
+
+    // assert
+    expect(newState.length).toEqual(3);
+    expect(newState[0].title).toEqual('A');
+    expect(newState[1].title).toEqual('B');
+    expect(newState[2].title).toEqual('C');
+  });
+
+  it('should update course when passed ' + types.UPDATE_COURSE_SUCCESS, () => {
+    // arrange
+    const initialState = [
+      {id: 'A', title: 'A'},
+      {id: 'B', title: 'B'},
+      {id: 'C', title: 'C'}
+    ];
+    const newTitle = 'New Title';
+    const course = {id: 'B', title: newTitle};
+    const action = actions.updateCourseSuccess(course);
+
+    // act
+    const newState = courseReducer(initialState, action);
+    const updatedCourse = newState.find(c => c.id == course.id);
+    const untouchedCourse = newState.find(c => c.id == 'A');
+
+    // assert
+    expect(newState.length).toEqual(3);
+    expect(updatedCourse.title).toEqual(newTitle);
+    expect(untouchedCourse.title).toEqual('A');
+  });
+});
