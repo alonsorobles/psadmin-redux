84691a456a1e5d525b1fe9412e7504d5174ea467
Alonso Robles <alonso@alonsorobles.com>
2017-03-12 12:35:46 -0500

    Changed == to ===

--- src/api/delay.js
+++ src/api/delay.js
@@ -1,2 +1,2 @@
 // Consider setting the mock api delay based on whether we are developing (1000) or testing (0).
-export default process.env.NODE_ENV == 'test' ? 0 : 1000;
+export default process.env.NODE_ENV === 'test' ? 0 : 1000;
