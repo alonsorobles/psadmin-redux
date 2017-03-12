fcd7a5df803f9b73ed03118d3b753f2d1584499e
Alonso Robles <alonso@alonsorobles.com>
2017-03-12 14:43:25 -0500

    Referenced authorActions

--- src/components/author/AuthorsPage.js
+++ src/components/author/AuthorsPage.js
@@ -1,6 +1,7 @@
 import React, {PropTypes} from "react";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
+import * as actions from "../../actions/authorActions";
 
 class AuthorsPage extends React.Component {
     constructor(props, context) {
