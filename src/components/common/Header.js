056e08b97638624594af41c2452ce4db31217cfe
Alonso Robles <alonso@alonsorobles.com>
2017-03-13 00:53:11 -0500

    Some additional styling

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,11 +8,11 @@ const Header = ({loading, courseCount, authorCount}) => {
         <ul className="nav navbar-nav">
           <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
           <li><Link to="/courses" activeClassName="active">Courses&nbsp;
-              {!loading && <span className="badge">{courseCount}</span>}
-            </Link></li>
+            <span className="badge">{courseCount > 0 && courseCount}</span>
+          </Link></li>
           <li><Link to="/authors" activeClassName="active">Authors&nbsp;
-            {!loading && <span className="badge">{authorCount}</span>}
-            </Link></li>
+            <span className="badge">{authorCount > 0 && authorCount}</span>
+          </Link></li>
           <li><Link to="/about" activeClassName="active">About</Link></li>
           {loading &&
           <li><p className="navbar-text"><i className="fa fa-refresh fa-spin fa-fw" aria-hidden="true"/> Loading</p>
